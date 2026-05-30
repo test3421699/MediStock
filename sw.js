71ba0b16106679725c6010649cebe053e75a1764
test3421699 <test91674070@gmail.com>
2026-05-30 15:46:22 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "medistock-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
